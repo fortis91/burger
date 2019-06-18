40c5d1cc51405fc1a9ed054323be26751dc04e05
Nigel <neastern101@yahoo.com>
2019-06-18 10:13:41 -0500

    update seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,6 @@ INSERT INTO burgers (burger_name, devoured) VALUES('Bacon Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES('Avocodo Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES('Veggie Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES('Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Ultimate Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Big Mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Whopper', false);
